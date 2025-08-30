ad5e77e9ba8eb42bda431499a681e72bca47477e
wetw0rk <mvalen1997@gmail.com>
2025-08-29 22:23:41 -0500

    Pushing changes to processthreadsapi.py

--- src/sickle/common/headers/windows/processthreadsapi.py
+++ src/sickle/common/headers/windows/processthreadsapi.py
@@ -1,39 +1,77 @@
-from ctypes import c_int16
-from ctypes import c_int32
-from ctypes import Structure
+##
+# References:
+#
+#   https://github.com/wine-mirror/wine/blob/1d1e5fb3e51b2acb0143e86c16463dfed1bc90aa/include/processthreadsapi.h
+#   https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-startupinfoa
+#   https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-process_information
+#
+##
+
+import enum
+import ctypes
 
 from sickle.common.lib.reversing.smartarch import get_ptr
 
 STARTF_USESTDHANDLES = 0x00000100
 
-class _PROCESS_INFORMATION(Structure):
+class _THREAD_INFORMATION_CLASS(enum.IntEnum):
+    ThreadMemoryPriority = 0
+    ThreadAbsoluteCpuPriority = 1
+    ThreadDynamicCodePolicy = 2
+    ThreadPowerThrottling = 3
+    ThreadInformationClassMax = 4
+
+class _MEMORY_PRIORITY_INFORMATION(ctypes.Structure):
     _fields_ = [
-        ("hProcess",    get_ptr()),
-        ("hThread",     get_ptr()),
-        ("dwProcessId", c_int32),
-        ("dwThreadId",  c_int32),
+        ("MemoryPriority", ctypes.c_uint32), # ULONG
     ]
 
-class _STARTUPINFOA(Structure):
+class _THREAD_POWER_THROTTLING_STATE(ctypes.Structure):
     _fields_ = [
-        ("cb",              c_int32),
-        ("lpReserved",      get_ptr()),
-        ("lpDesktop",       get_ptr()),
-        ("lpTitle",         get_ptr()),
-        ("dwX",             c_int32),
-        ("dwY",             c_int32),
-        ("dwXSize",         c_int32),
-        ("dwYSize",         c_int32),
-        ("dwXCountChars",   c_int32),
-        ("dwYCountChars",   c_int32),
-        ("dwFillAttribute", c_int32),
-        ("dwFlags",         c_int32),
-        ("wShowWindow",     c_int16),
-        ("cbReserved2",     c_int16),
-        ("lpReserved2",     get_ptr()),
-        ("hStdInput",       get_ptr()),
-        ("hStdOutput",      get_ptr()),
-        ("hStdError",       get_ptr()),
+        ("Version",     ctypes.c_uint32), # ULONG
+        ("ControlMask", ctypes.c_uint32), # ULONG
+        ("StateMask",   ctypes.c_uint32), # ULONG
     ]
 
+class _QUEUE_USER_APC_FLAGS(enum.IntEnum):
+    QUEUE_USER_APC_FLAGS_NONE = 1
+    QUEUE_USER_APC_FLAGS_SPECIAL_USER_APC = 0x00000001
+    QUEUE_USER_APC_CALLBACK_DATA_CONTEXT = 0x00010000
 
+class _APC_CALLBACK_DATA(ctypes.Structure):
+    _fields_ = [
+        ("Parameter",     get_ptr()), # ULONG_PTR 
+        ("ContextRecord", get_ptr()), # CONTEXT *
+        ("Reserved0",     get_ptr()), # ULONG_PTR
+        ("Reserved1",     get_ptr()), # ULONG_PTR
+    ]
+
+class _PROCESS_INFORMATION(ctypes.Structure):
+    _fields_ = [
+        ("hProcess",    get_ptr()),       # HANDLE
+        ("hThread",     get_ptr()),       # HANDLE
+        ("dwProcessId", ctypes.c_uint32), # DWORD
+        ("dwThreadId",  ctypes.c_uint32), # DWORD
+    ]
+
+class _STARTUPINFOA(ctypes.Structure):
+    _fields_ = [
+        ("cb",              ctypes.c_uint32), # DWORD
+        ("lpReserved",      get_ptr()),       # LPSTR
+        ("lpDesktop",       get_ptr()),       # LPSTR
+        ("lpTitle",         get_ptr()),       # LPSTR
+        ("dwX",             ctypes.c_uint32), # DWORD
+        ("dwY",             ctypes.c_uint32), # DWORD
+        ("dwXSize",         ctypes.c_uint32), # DWORD
+        ("dwYSize",         ctypes.c_uint32), # DWORD
+        ("dwXCountChars",   ctypes.c_uint32), # DWORD
+        ("dwYCountChars",   ctypes.c_uint32), # DWORD
+        ("dwFillAttribute", ctypes.c_uint32), # DWORD
+        ("dwFlags",         ctypes.c_uint32), # DWORD
+        ("wShowWindow",     ctypes.c_int16),  # WORD
+        ("cbReserved2",     ctypes.c_int16),  # WORD
+        ("lpReserved2",     get_ptr()),       # LPBYTE
+        ("hStdInput",       get_ptr()),       # HANDLE
+        ("hStdOutput",      get_ptr()),       # HANDLE
+        ("hStdError",       get_ptr()),       # HANDLE
+    ]
