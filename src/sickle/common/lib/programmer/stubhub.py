bbee3fc1dfb9d61b40051243c658b8fe6c62ac85
wetw0rk <mvalen1997@gmail.com>
2025-09-02 21:41:40 -0500

    Added documentation to function for epilogue

--- src/sickle/common/lib/programmer/stubhub.py
+++ src/sickle/common/lib/programmer/stubhub.py
@@ -29,6 +29,19 @@ def get_win_prologue(op_as_func, stack_space):
     return stub
 
 def get_win_epilogue(op_as_func, exit_technique, storage_offsets):
+    """This function will generate a generic function epilogue based on the flags provided
+    by the user.
+
+    :param op_as_func: Determines if the prologue will operate as a normal function
+    :type op_as_func: bool
+
+    :param exit_technique: The technique to use when exiting the shellcode
+    :type exit_technique: str
+
+    :param storage_offsets: Dictionary object containing the storage offsets for
+        a given target function.
+    :type storage_offsets: dict
+    """
 
     stub = ""
 
